Fix DQNP return computation bug, add extra DQNP parameters

env_wrapper.py
@@ -1,8 +1,8 @@
-from typing import Union, List, Tuple
+from typing import Union, Tuple
 import numpy as np
 import gym
 
-State = Union[List[float], Tuple[int]]
+State = Union[np.array, Tuple[int]]
 
 class StateInfo:
     def __init__(self, bins, low, high=None):
@@ -88,11 +88,13 @@ class EnvWrapper(gym.Env):
         self._env.render(*args, **kwargs)
 
     def solved(self, rewards: [float]) -> bool:
-        assert self.solve_runs is not None
         assert self.solve_threshold is not None
 
-        rewards = rewards[-100:]
-        return len(rewards) == 100 and rewards.mean() > self.solve_threshold
+        if self.solve_runs:
+            if len(rewards) < self.solve_runs:
+                return False
+            rewards = rewards[-self.solve_runs:]
+        return rewards.mean() > self.solve_threshold
 
     def _process_state(self, state: State) -> State:
         if self.normalize:

lunarlander.py
@@ -1,5 +1,3 @@
-import numpy as np
-import gym
 from env_wrapper import EnvWrapper, StateInfo
 
 
@@ -10,10 +8,10 @@ class LunarLander(EnvWrapper):
     actions: main engine | right engine | left engine | do nothing
     reward: combination of proximity to landing and close to zero speed
     """
-    solve_runs = 100
+    # solve_runs = 100
     solve_threshold = 200
 
-    def __init__(self, discretization='observed', concat_prev_state=True):
+    def __init__(self, discretization='observed'):
         if discretization == 'observed':
             state_infos = [
                 StateInfo(10, .65),       # x pos
@@ -30,41 +28,10 @@ class LunarLander(EnvWrapper):
         super().__init__(env_name='LunarLander-v2', state_infos=state_infos)
 
         # TODO feature selection: don't show last two state values?
-        self._concat_prev_state = concat_prev_state
-        if concat_prev_state:
-            low  = self._env.observation_space.low
-            high = self._env.observation_space.high
-            space = gym.spaces.Box(np.repeat(low, 2),
-                                   np.repeat(high, 2))
-            self.observation_space = space
-
-            self.prev_state = np.zeros(self.state_size)
-
-        else:
-            self.prev_state = None
-
-    @property
-    def config(self) -> dict:
-        c = super().config
-        c.update({attr: getattr(self, attr) for attr in [
-            '_concat_prev_state',
-        ]})
-        return c
-
-    def reset(self):
-        initial_state = super().reset()
-        self.prev_state = initial_state
-        blank_prev_state = np.zeros(self.state_size // 2)
-        return np.concatenate([blank_prev_state, initial_state])
 
     def step(self, action: int):
-        current_state, reward, done = super().step(action)
+        state, reward, done = super().step(action)
         if self.n_actions > 500:  # don't allow it to get stuck
             done = True
             reward -= 100
-        if self._concat_prev_state:
-            concatenated = np.concatenate([self.prev_state, current_state])
-            self.prev_state = current_state  # update in memory
-            return concatenated, reward, done
-        else:
-            return current_state, reward, done
+        return state, reward, done